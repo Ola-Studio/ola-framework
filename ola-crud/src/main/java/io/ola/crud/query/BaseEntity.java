3656a23f43d5dbabea20b5fdf4669700dccd5ac6
yiumankam <415481084@qq.com>
2023-08-01 18:27:55 +0800

    init: 初始化项目&增加common模块、crud模块

---
+++ ola-crud/src/main/java/io/ola/crud/query/BaseEntity.java
@@ -0,0 +1,20 @@
+package io.ola.crud.query;
+
+import com.mybatisflex.annotation.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author yiuman
+ * @date 2023/7/25
+ */
+@Data
+public class BaseEntity<KEY> {
+    @Id
+    private KEY id;
+    private String creator;
+    private String lastModifier;
+    private Date createTime;
+    private Date updateTime;
+}
---
+++ ola-crud/src/main/java/io/ola/crud/query/QueryField.java
@@ -0,0 +1,24 @@
+package io.ola.crud.query;
+
+import java.lang.annotation.*;
+
+/**
+ * @author yiuman
+ * @date 2023/7/26
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Inherited
+public @interface QueryField {
+
+    String method() default "EQUALS";
+
+    String mapping() default "";
+
+    Clauses clauses() default Clauses.AND;
+
+    boolean require() default false;
+
+    Class<? extends ConditionHandler> handler() default ConditionHandler.class;
+}
